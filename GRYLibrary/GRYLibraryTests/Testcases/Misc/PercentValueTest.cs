19667653bbd3512e0d4c7180a88545d5ab55dbcf
Marius Göcke <marius.goecke@gmail.com>
2026-07-21 19:20:19 +0200

    fix

---
+++ GRYLibrary/GRYLibraryTests/Testcases/Misc/PercentValueTest.cs
@@ -0,0 +1,37 @@
+using GRYLibrary.Core.Misc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GRYLibrary.Tests.Testcases.Misc
+{
+    [TestClass]
+    public class PercentValueTest
+    {
+        [TestMethod]
+        [TestProperty(nameof(TestKind), nameof(TestKind.UnitTest))]
+        public void DivisionByDoubleTest()
+        {
+            //arrange
+            var value = new PercentValue(0.5);
+
+            //act
+            PercentValue result = value / 2.0;
+
+            //assert
+            Assert.AreEqual(0.25m, result.Value);
+        }
+
+        [TestMethod]
+        [TestProperty(nameof(TestKind), nameof(TestKind.UnitTest))]
+        public void DivisionOfDoubleByPercentValueTest()
+        {
+            //arrange
+            var value = new PercentValue(0.5);
+
+            //act
+            PercentValue result = 0.25 / value;
+
+            //assert
+            Assert.AreEqual(0.5m, result.Value);
+        }
+    }
+}

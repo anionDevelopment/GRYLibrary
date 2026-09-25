3a0d2d55c4812d939060989993a30316cbc09de6
Marius Göcke <marius.goecke@gmail.com>
2026-09-25 18:53:56 +0200

    added IteratingBackgroundService.RunAlsoWhenTheExecutionModeIsNotRunProgram

--- GRYLibrary/GRYLibrary/APIServer/BaseServices/IIteratingBackgroundService.cs
+++ GRYLibrary/GRYLibrary/APIServer/BaseServices/IIteratingBackgroundService.cs
@@ -4,6 +4,8 @@ namespace GRYLibrary.Core.APIServer.BaseServices
 {
     public interface IIteratingBackgroundService
     {
+        /// <summary>Whether the service also runs when the execution-mode is not <c>RunProgram</c>.</summary>
+        public bool RunAlsoWhenTheExecutionModeIsNotRunProgram { get; set; }
         public void StartAsync();
         public Task Stop();
     }
